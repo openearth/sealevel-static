de1bd05fbfe9d70a8f8518550f482c298fc23b89
Adriaanse <bart.adriaanse@gmail.com>
2018-08-08 09:59:02 +0200

    add layer information display to legend panel

--- src/components/VLayerControl.vue
+++ src/components/VLayerControl.vue
@@ -19,6 +19,7 @@
         <div class="ma-0 pl-5 pr-5">
           <v-slider v-if="layer.opacity" hide-details title="Change layer opacity" :min="1" :max="100" v-model="layer.opacity"></v-slider>
           <v-select v-if="layer.visualisations" :items="layer.visualisations" item-text="name" item-value="name" v-model="falseColor" item></v-select>
+          <div v-if="layer.info" class="pb-2">{{layer.info}}</div>
           <div v-if="layer.legend">
             <template v-if="layer.legend.range">
               <div class="color-ramp" v-if="layer.legend.colors" :style="colorRamp(layer.legend)" ></div>
