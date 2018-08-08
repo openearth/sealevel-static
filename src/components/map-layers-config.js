30a36ba0c17fdfc6efbcaa427d20483658aa38e1
Adriaanse <bart.adriaanse@gmail.com>
2018-08-08 09:59:46 +0200

    example data demonstrating layer legend options

--- src/components/map-layers-config.js
+++ src/components/map-layers-config.js
@@ -5,6 +5,11 @@ const mapLayers = [
     icon: '/images/eez.png',
     opacity: 100,
     active: false,
+    info: 'source: marineregions.org',
+    legend: {
+      colors: ['#ff0000'],
+      labels: ['EEZ boundary']
+    },
     data: [{
       'id': 'eez',
       'type': 'line',
@@ -14,7 +19,7 @@ const mapLayers = [
       },
       'source-layer': 'EEZ_boundary-8t9rn6',
       'paint': {
-        'line-color': 'rgba(256, 0, 0, 0.75)'
+        'line-color': 'rgba(256, 0, 0, 1.0)'
       }
     }]
   },
@@ -24,6 +29,11 @@ const mapLayers = [
     icon: '/images/24miles.png',
     opacity: 100,
     active: true,
+    info: 'source: marineregions.org',
+    legend: {
+      colors: ['#404040', '#c8c8c8'],
+      labels: ['zone', 'boundary']
+    },
     data: [{
       'id': '24miles',
       'type': 'fill',
@@ -33,8 +43,8 @@ const mapLayers = [
       },
       'source-layer': '24_miles_zone-9d814s',
       'paint': {
-        'fill-color': 'rgba(64, 64, 64, 0.5)',
-        'fill-outline-color': 'rgba(255, 255, 255, 0.5)'
+        'fill-color': 'rgba(64, 64, 64, 1.0)',
+        'fill-outline-color': 'rgba(200, 200, 200, 1.0)'
       }
     }]
   },
@@ -44,6 +54,11 @@ const mapLayers = [
     icon: '/images/12miles.png',
     opacity: 100,
     active: true,
+    info: 'source: marineregions.org',
+    legend: {
+      colors: ['#404040', '#c8c8c8'],
+      range: '0 1'
+    },
     data: [{
       'id': '12miles',
       'type': 'fill',
@@ -54,7 +69,7 @@ const mapLayers = [
       'source-layer': '12_miles_zone-d5cbg5',
       'paint': {
         'fill-color': 'rgba(64, 64, 64, 0.5)',
-        'fill-outline-color': 'rgba(255, 255, 255, 0.5)'
+        'fill-outline-color': 'rgba(200, 200, 200, 1.0)'
       }
     }]
   }
