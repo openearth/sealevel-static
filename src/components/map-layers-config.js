c734a6086e311ae89cc546a5341d93e59b07665a
Adriaanse <bart.adriaanse@gmail.com>
2018-09-10 16:32:03 +0200

    add SSH legend

--- src/components/map-layers-config.js
+++ src/components/map-layers-config.js
@@ -2,14 +2,14 @@ const mapLayers = [
   {
     id: 'sshtrend',
     layerType: 'gee',
-    name: 'SSH trend',
+    name: 'SSH Trend',
     icon: '/images/sshtrend.png',
     opacity: 100,
     active: true,
     info: '<i>source: <a href="https://podaac.jpl.nasa.gov/" target="_blank">NASA poodaac</a></i>',
     legend: {
-      colors: ['#000000', '#ffffff'],
-      range: '0 1'
+      colors: ['#151d44', '#156c72', '#7eb390', '#fdf5f4', '#db8d77', '#9c3060', '#340d35'],
+      range: '-0.03 [m] +0.03'
     },
     serviceUrl: 'http://hydro-engine.appspot.com/get_sea_surface_height_trend_image',
     data: []
